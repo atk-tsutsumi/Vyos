6f21c4c63e82aaf5377e1d97ee505a6ccf9baec8
Atsuki <d31423at@apps.kct.ac.jp>
2019-01-16 14:14:54 +0900

    configのsh作るshとそれを消すsh作った

--- work/build.sh
+++ work/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-for i in `seq 1 4`
+for i in `seq 1 10`
 do
   docker build --build-arg CONTAINER_ID=$i -t vyos$i .
 done
---
+++ work/makesh.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+for i in `seq 1 10`
+do
+  touch vyos$i.sh
+  echo "#!/bin/vbash" > vyos$i.sh
+  echo -e "\${vyatta_sbindir}/my_set interfaces loopback lo address $i.$i.$i.$i/32" >> vyos$i.sh
+  echo -e "\${vyatta_sbindir}/my_set protocols ospf area 0 network 10.0.0.0/16" >> vyos$i.sh
+  echo -e "\${vyatta_sbindir}/my_set protocols ospf parameters router-id $i.$i.$i.$i" >> vyos$i.sh
+  echo -e "\${vyatta_sbindir}/my_commit" >> vyos$i.sh
+done
