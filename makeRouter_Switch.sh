cf63de82e4c9da573cfa54cfbd2b078c38150fbc
atk-tsutsumi <45929457+atk-tsutsumi@users.noreply.github.com>
2019-03-15 12:08:42 +0900

    Update makeRouter_Switch.sh

--- makeRouter_Switch.sh
+++ makeRouter_Switch.sh
@@ -2,7 +2,7 @@
 for i in `seq 1 10`
 do
   #vyosコンテナスタート
-  docker run -d --name vyos$i --privileged --net=none -v /lib/modules:/lib/modules vyos$i /sbin/init
+  sudo docker run -d --name vyos$i --privileged --net=none -v /lib/modules:/lib/modules vyos$i /sbin/init
   sudo ovs-vsctl del-br switch$i
   sudo ovs-vsctl add-br switch$i
 done
